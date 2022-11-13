21ef36e67cc136834bd9753145ccada989d6b702
unknown <mateuszkarpeta@gmail.com>
2022-11-13 22:43:27 +0100

    first init

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f5aaed5f116c336cf110651a00f17d8e&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback("Unable to connecto to location services!", undefined)
+        } else if (body.error) {
+            callback("Weatherstack internal error.\nError code: " + body.error.code , undefined)
+        } else {
+            callback(undefined, 'It is now ' + body.current.temperature + ' deg, and it feels like ' + body.current.feelslike + ' deg'
+        )
+        }
+    })
+}
+
+module.exports = forecast
